dff594c3a194329915551b246e5bd6e74e483bcd
Placius <krystekpol@tlen.pl>
2021-01-08 17:09:29 +0100

    create AppMenu class with functions

--- fit_progress.py
+++ fit_progress.py
@@ -6,33 +6,81 @@
 # git push -u origin main
 
 # import modules
-import sys
+import sys, os
 
 # app menu
 
 class AppMenu():
     
     def __init__(self):   
-        exercises_list = ["exercise1","exercise2","exercise3","exercise4",]   
+        exercises_list = ["exercise1","exercise2","exercise3","exercise4","exercise5"]   
         self.title = "FIT progress"
-        self.exercises = exercises_list
         self.tracking_progress_function = "Track your progress"
-        self.exit = "sys.exit(0)"
+        self.exercises = exercises_list
+        self.user_choice = None
+        self.all_moves = {}
+    
+    def CreateAllMovesList(self):
+        keys = 1
+        self.all_moves[keys] = [self.tracking_progress_function]
+        keys += 1
+        for exercise in self.exercises:
+            self.all_moves[keys] = [exercise]
+            keys += 1
+        self.all_moves[keys] = ["exit"]
+
+        print(self.all_moves)
+        
 
     def ShowAll(self):
         numerate = 1
         print(self.title, "\n")
-        for exercise in self.exercises:
-            print(numerate, " ", exercise)
-            numerate +=1
-        print(numerate, " ", self.tracking_progress_function)
+        print(numerate, " - ", self.tracking_progress_function)
         numerate +=1
+        for exercise in self.exercises:
+            print(numerate, " - ", exercise)
+            numerate +=1
         print("\n",numerate, "Exit")
-    
-    def MakeAchoice(self):
-        pass
+
+
+    def MakeAchoice(self, show_menu):
+        while True:
+            # auto clean window after new entry
+            os.system("cls")
+
+            # print menu option
+            show_menu()
+
+            # choices 
+            user_choice = input("What do you want now to do? - ")
+            self.user_choice = user_choice
+            if self.user_choice == '1':
+                pass
+            
+            elif self.user_choice == '2':
+                pass
+
+            elif self.user_choice == '3':
+                pass
+
+            elif self.user_choice == '4':
+                pass
+
+            elif self.user_choice == '5':
+                pass
+            
+            # auto numbered for exit function 
+
+            elif self.user_choice == str(len(self.all_moves)):
+                print("Bye Bye...")
+                sys.exit(0)
+            
+            else:
+                print("Somthing goes wrong, please try again!")
 
 
 startApp = AppMenu()
 
-startApp.ShowAll()
+startApp.CreateAllMovesList()
+
+startApp.MakeAchoice(startApp.ShowAll)
