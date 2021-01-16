7ac1e1fc18f56a8c839d157beca4668803c6b9a4
Placius <krystekpol@tlen.pl>
2021-01-16 16:30:14 +0100

    create datas in files and exercise function actualisation

--- exercises.py
+++ exercises.py
@@ -5,28 +5,76 @@ import time, os, winsound
 
 # general class for exercises classes
 class Exercise:
-    def __init__(self, name, description, lvl, repeat, series, weekly_plan, pause):
+    def __init__(self, name, pause, lvl):
         self.name = name
-        self.description = "descroiption"
+        self.description = "description"
         self.lvl = lvl
-        self.series = series
-        self.repeat = repeat
-        self.pause_time_after_one_serie = pause
+        self.series = "series"
+        self.repeat = "repeat"
+        self.pause = pause
+        self.week = "week"
+
+        self.How_much_series_and_repeats()
 
     # matching the task to the exercise
     def How_much_series_and_repeats(self):
+        while True:
+            week = int(input("Training week -"))
+            if week < 1 or week > 10:
+                print("Bad command, please try again.")
+            
+            else:
+                self.week = week
+                break
 
         if self.lvl == "Beginner":
-            pass
+            lista =[]
+            with open("training_plans/begginer/week"+str(self.week)+".txt") as file:
+                lines = file.read().split("\n")
+                for line in lines:
+                    lista.append(line)
+            
+            self.series = int(lista[0])
+            self.repeat = int(lista[1])
         
         elif self.lvl == "Low":
-            pass
+            if self.week > 8:
+                self.week = 8
+
+            lista =[]
+            with open("training_plans/low/week"+str(self.week)+".txt") as file:
+                lines = file.read().split("\n")
+                for line in lines:
+                    lista.append(line)
+            
+            self.series = int(lista[0])
+            self.repeat = int(lista[1])
 
         elif self.lvl == "Medium":
-            pass
+            if self.week > 6:
+                self.week = 6
+                
+            lista =[]
+            with open("training_plans/medium/week"+str(self.week)+".txt") as file:
+                lines = file.read().split("\n")
+                for line in lines:
+                    lista.append(line)
+            
+            self.series = int(lista[0])
+            self.repeat = int(lista[1])
 
         elif self.lvl == "High":
-            pass
+            if self.week > 4:
+                self.week = 4
+                
+            lista =[]
+            with open("training_plans/high/week"+str(self.week)+".txt") as file:
+                lines = file.read().split("\n")
+                for line in lines:
+                    lista.append(line)
+            
+            self.series = int(lista[0])
+            self.repeat = int(lista[1])
         
         else:
             pass
@@ -77,9 +125,19 @@ class WarmUp:
     
 # push-ups
 class Exercise1(Exercise):
-    def __init__(self):    
-        pass
+    def __init__(self, lvl):   
+        lista =[]
+        with open("exercises/push-up.txt") as file:
+            lines = file.read().split("\n")
+        
+            for line in lines:
+                lista.append(line)
 
+        super().__init__(lista[0], lista[1], lvl)
+
+    def PrintInfos(self):
+        print(self.name, self.description, self.lvl, self.series, self.repeat, self.pause)
+    
 # pull-up
 class Exercise2(Exercise):
     def __init__(self):
@@ -100,4 +158,6 @@ class Exercise5(Exercise):
     def __init__(self):
         pass
 
-WarmUp()
+ex = Exercise1("Beginner")
+
+ex.PrintInfos()
